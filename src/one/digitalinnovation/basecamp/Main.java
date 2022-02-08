3c87af6ba78027e887f1741320a4f1080388055e
Sostenes <sostenesmartins1996@gmail.com>
2022-02-08 16:00:52 -0300

    Sobrecarga

--- src/one/digitalinnovation/basecamp/Main.java
+++ src/one/digitalinnovation/basecamp/Main.java
@@ -22,5 +22,12 @@ public class Main {
         Emprestimo.calcular(1000, Emprestimo.getDuasParcelas());
         Emprestimo.calcular(1000, Emprestimo.getTresParcelas());
         Emprestimo.calcular(1000, 5);
+
+        //Quadrilatero
+        System.out.println("Exercício quadrilatero");
+        Quadrilatero.area(3);
+        Quadrilatero.area(5d,5d);
+        Quadrilatero.area(7,8,9);
+        Quadrilatero.area(5f,5f);
     }
 }
